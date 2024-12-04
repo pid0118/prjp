68a451f58b76c4fdf013bcbbe4686e6b2489a13f
pid <dlsemr7137@naver.com>
2024-12-04 10:43:43 +0900

    first commit

---
+++ src/test/java/com/example/damo/PrjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.damo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
